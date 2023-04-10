556118648e235628314d859ea03ee43afc00fbec
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-10 19:01:23 +0200

    ajout exo_09

--- exo_09.rb
+++ exo_09.rb
@@ -1,5 +1,9 @@
 puts "quelle est votre annee de naissance"
-user_dayOfBirth = gets.chomp.to_i
-current_years = 
-
+birth_year = gets.chomp.to_i
+current_years = Time.now.year #class qui permet de definir l'annee actuelle
+(birth_year..current_years).each do |i| #methode each permet d'afficher tous les nombre entre deux variables
+    puts i  # puis imprime le resultat 
 end
+
+
+
