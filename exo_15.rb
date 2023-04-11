ca1db8f2c9dc1bafd41219e288c6af80d2fcfdb3
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-11 14:37:56 +0200

    modification pour rajouter conditions d'erreur

--- exo_15.rb
+++ exo_15.rb
@@ -2,9 +2,14 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 
 numbers_of_floors = gets.chomp.to_i
 
-# 1.upto {|i| print"#{i}"} methode upto
+if numbers_of_floors >= 1 && numbers_of_floors <= 26
 
 1.upto(numbers_of_floors) do |i| # premier iterateur pour le nombre de ligne afficher 
     i.times {print "#"} # la  3 eme pour le nombre de #
     puts
+end
+
+else   
+    puts "entrez un chiffre entre 1 et 25"
+      
 end
