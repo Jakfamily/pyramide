8fb224256bda5d1afd4d37d957f1cce8c17b5cb1
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-11 14:13:37 +0200

    ajout de commentaire

--- exo_09.rb
+++ exo_09.rb
@@ -5,8 +5,7 @@ birth_year = gets.chomp.to_i
 current_years = Time.now.year #class qui permet de definir l'annee actuelle
 
 (birth_year..current_years).each do |i| #methode each permet d'afficher tous les nombre entre deux variables
-    
-    puts i  # puis imprime le resultat 
+     puts i  # puis imprime le resultat 
 end
 
 
--- exo_11.rb
+++ exo_11.rb
@@ -4,10 +4,10 @@ age = gets.chomp.to_i #demande age utilisateur
 
 current_years = Time.now.year #class qui permet de definir l'annee actuelle
 
-birth_years = current_years - age #defini annee de naissance = annee actuelle - age
+birth_years = current_years - age #calcul annee de naissance en soustrayant age user et lanne actuelle
 
 age.times do |i|
-    year_age = current_years - birth_years - i # defini age = annee actuelle - annee de naissance - iteration 
+    year_age = current_years - birth_years - i #afficher un message pour chaque année depuis la naissance de l'utilisateur jusqu'à l'année actuelle en soustrayant date de naissance et soustrayant indice d'iteration
     
     puts "Il y a #{year_age} ans, tu avais #{i} ans"
 end
