9da48c1db7d476bd1370f99815ab39a36c226c0b
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-11 14:41:23 +0200

    ajout de commentaire

--- exo_07.rb
+++ exo_07.rb
@@ -2,6 +2,6 @@ puts "choisis un nombre"
 
 number = (gets.chomp.to_i)+ 1 # stock donnee utilisateur + incremente de +1 pour chaque passage de boucle
 
-number.times do |i| # boucle la variable avec la condition de + 1 
+number.times do |i| # permet d'incrementer +1 
     puts i  
 end
