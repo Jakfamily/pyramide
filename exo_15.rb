cdc7e2dc1a7847996a523b7bc1d5b5354063344e
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-12 11:27:11 +0200

    ajout de commentaire

--- exo_15.rb
+++ exo_15.rb
@@ -4,12 +4,14 @@ numbers_of_floors = gets.chomp.to_i
 
 if numbers_of_floors >= 1 && numbers_of_floors <= 26
 
-1.upto(numbers_of_floors) do |i| # premier iterateur pour le nombre de ligne afficher 
-    i.times {print "#"} # la  3 eme pour le nombre de #
-    puts
+1.upto(numbers_of_floors) do |i| 
+     # i représente le numéro de la ligne en cours
+    i.times {print "#"} # Affichage du nombre de "#" correspondant au numéro de ligne
+    puts #allez a la ligne
 end
 
-else   
+else  
+     # Si le nombre d'étages n'est pas compris entre 1 et 26 inclus, afficher un message d'erreur 
     puts "entrez un chiffre entre 1 et 25"
       
 end
