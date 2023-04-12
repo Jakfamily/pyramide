54ba0ec0f6e01e5efa23e66d366b5c78a3374b50
faria jean-baptiste <fariajeanbap@gmail.com>
2023-04-12 10:58:19 +0200

    correction

--- exo_13.rb
+++ exo_13.rb
@@ -1,14 +1,13 @@
-emails = [] #creation dun array stocker dans une variable
+emails= Array.new #creation dun array stocker dans une variable
 
 50.times do |i| #boucles times pour cree 50 emails 
 
   if i < 9 #conditions ajoute un zero avant le num d'email si il et inferieur a 10
-    email = "jean.dupont.0#{i + 1}@email.fr"
+    emails.push "jean.dupont.0#{i + 1}@email.fr"
 
   else #sinon itere +1 au numero d'email
-    email = "jean.dupont.#{i + 1}@email.fr"
+    emails.push "jean.dupont.#{i + 1}@email.fr"
   end
-  =
 end
 
 puts emails
